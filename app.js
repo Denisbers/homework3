d1f552a39e86cc61aea8fdb2d8568b0aea423e5d
Denisbers <denis16031984@gmail.com>
2022-10-05 22:03:28 +0300

    commit

---
+++ app.js
@@ -0,0 +1,13 @@
+let digit1 = prompt('Enter number 1')
+
+let digit2 = prompt('Enter number 2')
+
+let digit3 = prompt('Enter number 3')
+
+digit1 = parseInt(digit1)
+digit2 = parseInt(digit2)
+digit3 = parseInt(digit3)
+
+alert(((digit1) + (digit2) + (digit3) / 3));
+console.log(alert);
+
